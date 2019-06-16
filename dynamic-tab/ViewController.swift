16370dc132b3a1b518efd507f3479625428eedff
David Wibisono <davidwibisono95@gmail.com>
2019-06-16 15:14:51 +0700

    Initial Commit

---
+++ dynamic-tab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dynamic-tab
+//
+//  Created by David-UC on 6/16/19.
+//  Copyright © 2019 David-DAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
